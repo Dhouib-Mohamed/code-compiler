fd091c8a775052edba2da472e38fc8277527a0ae
Dhouib25 <mohamed.dhouib@insat.ucar.tn>
2022-08-10 20:25:46 +0100

    update

--- data/languages.js
+++ data/languages.js
@@ -21,7 +21,7 @@ export const languages = [
             '  char *buffer = NULL;',
             '  long unsigned int len;',
             '  getline(&buffer, &len, stdin);',
-            '  printf("%s\\n", buffer);',
+            '  printf("%s\ ", buffer);',
             '  free(buffer);',
             '  return 0;',
             '}'
@@ -36,10 +36,10 @@ export const languages = [
             'using namespace std;',
             '',
             'int main(int argc, char **argv) {',
-            '  cout << "Hello World from C++!\\n";',
+            '  cout << "Hello World from C++!\ ";',
             '  string mystr;',
             '  getline(cin, mystr);',
-            '  cout << mystr << "\\n";',
+            '  cout << mystr << "\ ";',
             '  return 0;',
             '}'
         ].join('\n'))
@@ -57,6 +57,42 @@ export const languages = [
             '    Scanner scan = new Scanner(System.in);',
             '    System.out.println(scan.nextLine());',
             '  }',
+            '}'
+        ].join('\n'))
+    },
+    {
+        name:"Go",
+        compileName:"go",
+        extention: ".go",
+        code: ([
+            'package main',
+        '',
+        'import "fmt"','',
+
+        'func main() {',
+        '    fmt.Println("Hello World From GoLand!")',
+    '    var first string' ,
+    '    fmt.Scanln(&first)' ,
+    '    fmt.Println(first)' ,
+    '',
+    '}'
+        ].join('\n'))
+    },
+    {
+        name:"C-Sharp",
+        compileName:"cs",
+        extention: ".cs",
+        code: ([
+            'using System;' ,
+            'using System.Collections.Generic;' ,
+            ' ' ,
+            'class HelloWorld { ' ,
+            '   static void Main() { ' ,
+            '      string val; ' ,
+            '      Console.Write("Hello World From C#!"); ' ,
+            '      val = Console.ReadLine(); ' ,
+            '      Console.WriteLine(val); ' ,
+            '   } ' ,
             '}'
         ].join('\n'))
     },
